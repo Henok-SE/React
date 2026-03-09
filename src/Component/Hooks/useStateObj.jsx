4d2002ddd3ef12deeb4b929507ca6fc56dfe9069
Henok Assefa <assefahenok33@gmail.com>
2026-03-09 11:41:35 -0700

    update

--- src/Component/Hooks/useStateObj.jsx
+++ src/Component/Hooks/useStateObj.jsx
@@ -3,6 +3,10 @@ import { useState } from 'react'
 
 function UseStateObj() {
     const [name, setName] = useState({FName: "", LName: ""});
+    const [isVisible, setVisible]= useState(false);
+    // function toggleMessage(){
+    //     setVisible(!isVisible);
+    // }
   return (
     <div>
       <form>
@@ -11,8 +15,15 @@ function UseStateObj() {
         <h2>Your First name is  {name.FName}</h2>
         <h2>Your Last name is  {name.LName}</h2>
       </form>
+      <button id="toggle-btn" onClick={()=>setVisible(!isVisible)}>Message</button>
+       {isVisible &&  <p> I love coding </p>}
+
     </div>
   )
 }
 
 export default UseStateObj
+
+
+
+
